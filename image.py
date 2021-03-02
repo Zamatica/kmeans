a2483b126c276b366962dba92651543c547bc360
sweet <sweet@localhost>
2021-03-01 21:37:48 -0500

    [IMAGE] Added image class to read in a file

--- image.py
+++ image.py
@@ -1,15 +1,21 @@
 
 import cv2
+import point
 
 
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+class Image:
+    def __init__(self, filename) -> None:
+        self.image = cv2.imread(filename=filename)
+        self.gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
-# img = cv2.imread('D:\Project\Capture1.jpg',0)
-# rows,cols = img.shape
-# 
-#     for i in range(rows):
-#       for j in range(cols):
-#          k = img[i,j]
-#         print k
+    def at(self, x, y):
+        return self.image[x, y]
 
+    def at_point(self, point: point.Point3D):
+        return self.image[point.x, point.y]
 
+    def at_gray(self, x, y):
+        return self.gray[x, y]
+
+    def at_gray_point(self, point: point.Point3D):
+        return self.gray[point.x, point.y]
--- point.py
+++ point.py
@@ -7,11 +7,11 @@ class Point3D:
         self.y = 0
         self.z = 0
 
-    def distance(self, point: Point3D) -> float:
+    def distance(self, point: 'Point3D') -> float:
         return (self.x - point.x) * (self.x - point.x) + \
                (self.y - point.y) * (self.y - point.y) + \
                (self.z - point.z) * (self.z - point.z)
 
-    def distance_sqrt(self, point: Point3D) -> float:
+    def distance_sqrt(self, point: 'Point3D') -> float:
         return np.sqrt(self.distance(point))
 
